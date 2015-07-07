f865d04d8562a5654b00258f7f5da592e871754e
Mark Luetke <luetkemj@gmail.com>
2015-07-07 13:21:52 -0400

    init

---
+++ includes/class-altlab-bensonstats-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://luetkemj.github.io
+ * @since      1.0.0
+ *
+ * @package    Altlab_Bensonstats
+ * @subpackage Altlab_Bensonstats/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Altlab_Bensonstats
+ * @subpackage Altlab_Bensonstats/includes
+ * @author     Mark Luetke <luetkemj@gmail.com>
+ */
+class Altlab_Bensonstats_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-altlab-bensonstats-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://luetkemj.github.io
+ * @since      1.0.0
+ *
+ * @package    Altlab_Bensonstats
+ * @subpackage Altlab_Bensonstats/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Altlab_Bensonstats
+ * @subpackage Altlab_Bensonstats/includes
+ * @author     Mark Luetke <luetkemj@gmail.com>
+ */
+class Altlab_Bensonstats_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
